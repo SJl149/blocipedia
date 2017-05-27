1271b62cff4417d96517c9f08e39ebe44916bf2f
Scott Lenander <sjlteacher@gmail.com>
2017-05-26 20:58:43 -0400

    Collaborators can be added

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -1,17 +1,28 @@
 class CollaborationsController < ApplicationController
 
   def create
-    user = User.find(params[:id])
-    collaboration = wiki.collaborations.build(user: user)
+    wiki = Wiki.find(params[:wiki_id])
+    user = User.find(params[:user_id])
+    Collaboration.create(user_id: user.id, wiki_id: wiki.id)
 
-    if collaboration.save
+    if Collaboration.create(user_id: user.id, wiki_id: wiki.id)
       flash[:notice] = "Collaboration saved."
     else
       flash[:alert] = "Collaboration failed."
     end
+    redirect_to edit_wiki_path(wiki)
   end
 
   def destroy
+    wiki = Wiki.find(params[:wiki_id])
+    user = User.find(params[:user_id])
+    @collaboration = Collaboration.find(user_id: user.id, wiki_id: wiki.id)
 
+    if @collaboration.destroy
+      flash[:notice] = "#{user.email} was removed successfully."
+    else
+      flash.now[:alert] = 'There was a problem removing the user.'
+    end
+    redirect_to edit_wiki_path(wiki)
   end
 end
--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,5 +2,5 @@ class Wiki < ActiveRecord::Base
   belongs_to :user
 
   has_many :collaborations
-  has_many :collaborating_users, through: :collaborations
+  has_many :collaborating_users, through: :collaborations, source: :user
 end
