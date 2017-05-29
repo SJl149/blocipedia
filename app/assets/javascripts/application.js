77878b7d1dd0957d648093e30c23e6c8749e6f0a
Scott Lenander <sjlteacher@gmail.com>
2017-05-29 18:06:12 -0400

    Added live preview to new and edit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,16 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+document.addEventListener('turbolinks:load', function() {
+  var converter = new showdown.Converter();
+  converter.setOption('tables', 'true');
+  var textField = document.querySelector('#wiki_body');
+  var preview = document.querySelector('#wiki-preview');
+
+  textField.addEventListener('keyup', function() {
+    var mdown = textField.value;
+    var html = converter.makeHtml(mdown);
+    preview.innerHTML = html;
+  })
+})
